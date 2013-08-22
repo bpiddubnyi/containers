c612d511b87c993d1910a646288e0fd7e8ac5748
Borys Piddubnyi <zhu@zhu.org.ua>
2013-08-22 16:46:03 +0300

    Reworked list unit test with new UT-framework

--- list_test.c
+++ list_test.c
@@ -1,12 +1,11 @@
 #include <string.h>
-#include <stdio.h>
-#include <assert.h>
 
+#include "unit_test.h"
 #include "list.h"
 
 #define NODES_NUM 1024
 
-int main() {
+UNIT_START
 	node_t nodes[NODES_NUM];
 	int data[NODES_NUM];
 	list_t list;
@@ -16,41 +15,41 @@ int main() {
 	memset(&list, 0, sizeof(list_t));
 	memset(nodes, 0, sizeof(node_t) * NODES_NUM);
 
-	printf("    list_push_front()");
+	TEST_START("list_push_front()");
 	for (i = 0; i < NODES_NUM; ++i) {
 		data[i] = i;
 		nodes[i].data = data + i;
 		list_push_front(&list, nodes + i);
-		assert((int)list.size == i + 1);
+		CHECK_EQUAL((int)list.size, i + 1);
 	}
-	puts("\tok");
+	TEST_END;
 	
-	printf("    list_pop_back()");
+	TEST_START("list_pop_back()");
 	for (i = 0; i < NODES_NUM; ++i) {
 		node_buf = list_pop_back(&list);
-		assert(*(int*)node_buf->data == i);
-		assert((int)list.size == NODES_NUM - (i + 1));
+		CHECK_EQUAL(*(int*)node_buf->data,  i);
+		CHECK_EQUAL((int)list.size, NODES_NUM - (i + 1));
 	}
-	assert(!list_pop_back(&list));
-	assert(!list_pop_front(&list));
-	puts("\tok");
+	CHECK_IF(!list_pop_back(&list));
+	CHECK_IF(!list_pop_front(&list));
+	TEST_END;
 	
-	printf("    list_push_back)");
+	TEST_START("list_push_back)");
 	for (i = 0; i < NODES_NUM; ++i) {
 		data[i] = i;
 		nodes[i].data = data + i;
 		list_push_back(&list, nodes + i);
-		assert((int)list.size == i + 1);
+		CHECK_EQUAL((int)list.size, i + 1);
 	}
-	puts("\tok");
+	TEST_END;
 	
-	printf("    list_pop_front()");
+	TEST_START("list_pop_front()");
 	for (i = 0; i < NODES_NUM; ++i) {
 		node_buf = list_pop_front(&list);
-		assert(*(int*)node_buf->data == i);
-		assert((int)list.size == NODES_NUM - (i + 1));
+		CHECK_EQUAL(*(int*)node_buf->data,  i);
+		CHECK_EQUAL((int)list.size, NODES_NUM - (i + 1));
 	}
-	assert(!list_pop_front(&list));
-	assert(!list_pop_back(&list));
-	puts("\tok");
-}
+	CHECK_IF(!list_pop_front(&list));
+	CHECK_IF(!list_pop_back(&list));
+	TEST_END;
+UNIT_END
